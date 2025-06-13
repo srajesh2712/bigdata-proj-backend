a034d098f7f781bb204b15e37359dbae27ceabc6
Rajesh subramanian <enstenr@gmail.com>
2025-06-13 12:24:38 +0530

    added code to directory

--- 1DownloadDataViaAPI/main.py
+++ 1DownloadDataViaAPI/main.py
@@ -14,6 +14,7 @@ response = requests.post(auth_url, data=payload)
 if response.status_code == 200:
     access_token = response.json().get("access_token")
     print("Authentication successful!")
+    print(access_token)
 else:
     raise Exception(f"Authentication failed: {response.text}")
 
@@ -22,7 +23,7 @@ api_url = "https://sh.dataspace.copernicus.eu/api/v1/process"  # Update this to 
 
 # Pass the token to SentinelAPI
 headers = {"Authorization": f"Bearer {access_token}"}
-search_url = "https://catalogue.dataspace.copernicus.eu/odata/v1/Products?$filter=..."
+search_url = "https://catalogue.dataspace.copernicus.eu/odata/v1/Products"
 
 response = requests.get(search_url, headers=headers)
 print(response.json())

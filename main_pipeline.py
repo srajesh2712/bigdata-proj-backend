65b8590d2e6c757ffebf137008f5fa1cbfd8833b
enstenr <enstenr@gmail.com>
2025-08-03 16:52:08 +0530

    working on pipeline

--- main_pipeline.py
+++ main_pipeline.py
@@ -2,9 +2,18 @@ import random
 
 from sar_pipeline.preprocessing.preprocess_sar import preprocess_sar_files
 from sar_pipeline.preprocessing.split_geotiff_files import split_files
+from sqlalchemy import create_engine
 
+engine = create_engine("postgresql+psycopg2://rajesh:rajesh@localhost/eo")
+from sqlalchemy.orm import sessionmaker
+Session = sessionmaker(bind=engine)
+session = Session()
+from sar_pipeline.schema.schema import SafeFile, Base
 from datetime import datetime
 import os
+def fetch_processing_files():
+    pending_files = session.query(SafeFile).filter_by(active=True, status='pending').all()
+    return pending_files
 if __name__ == '__main__':
 
     timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
@@ -12,18 +21,20 @@ if __name__ == '__main__':
     job_id=timestamp #'JOB_1'
     print("Job ID: ", job_id)
 
+
+    pending_files = fetch_processing_files()
     # step 1 - preprocess the sar file using graph xml and gpt command from snap
-    preprocess_sar_files(job_id)
+    preprocess_sar_files(job_id, pending_files)
 
     # step 2 - split the preprocessed files into chunks
-    sar_folders = ['S1A_IW_GRDH_1SDV_20250521T234717_20250521T234742_059299_075C07_507D.SAFE',
-                   'S1A_IW_GRDH_1SDV_20250602T234717_20250602T234742_059474_076219_9E58.SAFE']
+
     base_dir = '/home/btcchl0040/Documents/SAR_Data'
 
 
-    for file in sar_folders:
+    for file in pending_files:
+        folder_name = file.folder_path
         processed_geotiff= os.path.join(base_dir,f'{job_id}', 'PREPROCESSING',file , f'{file}_{job_id}.tif')
-        #timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+
         print(f'processed_geotiff  {file}','\n')
 
         parent_folder = os.path.dirname(processed_geotiff)

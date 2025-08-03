665250b34f62d8f2d296e87932a66f973f401a25
enstenr <enstenr@gmail.com>
2025-08-03 16:44:02 +0530

    working on pipeline

--- main_pipeline.py
+++ main_pipeline.py
@@ -1,13 +1,21 @@
 import random
-from ftplib import print_line
 
+from sar_pipeline.preprocessing.preprocess_sar import preprocess_sar_files
 from sar_pipeline.preprocessing.split_geotiff_files import split_files
+
 from datetime import datetime
 import os
 if __name__ == '__main__':
+
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
     # split files
-    job_id='JOB_1'
+    job_id=timestamp #'JOB_1'
     print("Job ID: ", job_id)
+
+    # step 1 - preprocess the sar file using graph xml and gpt command from snap
+    preprocess_sar_files(job_id)
+
+    # step 2 - split the preprocessed files into chunks
     sar_folders = ['S1A_IW_GRDH_1SDV_20250521T234717_20250521T234742_059299_075C07_507D.SAFE',
                    'S1A_IW_GRDH_1SDV_20250602T234717_20250602T234742_059474_076219_9E58.SAFE']
     base_dir = '/home/btcchl0040/Documents/SAR_Data'
@@ -15,7 +23,7 @@ if __name__ == '__main__':
 
     for file in sar_folders:
         processed_geotiff= os.path.join(base_dir,f'{job_id}', 'PREPROCESSING',file , f'{file}_{job_id}.tif')
-        timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+        #timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
         print(f'processed_geotiff  {file}','\n')
 
         parent_folder = os.path.dirname(processed_geotiff)
--- sar_pipeline/schema/schema.py
+++ sar_pipeline/schema/schema.py
@@ -1,6 +1,7 @@
 from sqlalchemy import Column, Integer, String, Boolean, DateTime, Text
-from sqlalchemy.ext.declarative import declarative_base
+
 from sqlalchemy.sql import func
+from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
 

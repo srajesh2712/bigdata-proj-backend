27438ad6720da7ffa3ca43440ced7ab273782874
enstenr <enstenr@gmail.com>
2025-08-03 16:22:21 +0530

    working on pipeline

--- main_pipeline.py
+++ main_pipeline.py
@@ -1,22 +1,24 @@
+import random
+from ftplib import print_line
+
 from sar_pipeline.preprocessing.split_geotiff_files import split_files
 from datetime import datetime
 import os
 if __name__ == '__main__':
     # split files
-    sar_folders = ['S1A_IW_GRDH_1SDV_20250521T234717_20250521T234742_059299_075C07_507D.SAFE'
+    job_id='JOB_1'
+    print("Job ID: ", job_id)
+    sar_folders = ['S1A_IW_GRDH_1SDV_20250521T234717_20250521T234742_059299_075C07_507D.SAFE',
                    'S1A_IW_GRDH_1SDV_20250602T234717_20250602T234742_059474_076219_9E58.SAFE']
     base_dir = '/home/btcchl0040/Documents/SAR_Data'
-    dir_list = ['OUTPUT/S1A_IW_GRDH_1SDV_20250521T234717_20250521T234742_059299_075C07_507D.SAFE/S1A_IW_GRDH_1SDV_20250521T234717_20250521T234742_059299_075C07_507D.SAFE_20250802_151438.tif',
-                'OUTPUT/S1A_IW_GRDH_1SDV_20250602T234717_20250602T234742_059474_076219_9E58.SAFE/S1A_IW_GRDH_1SDV_20250602T234717_20250602T234742_059474_076219_9E58.SAFE_20250802_151542.tif'
-                ]
 
 
     for file in sar_folders:
-        processed_geotiff= os.path.join(base_dir, file , 'OUTPUT', file, f)
+        processed_geotiff= os.path.join(base_dir,f'{job_id}', 'PREPROCESSING',file , f'{file}_{job_id}.tif')
         timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-        input_path = os.path.join(base_dir,files)
+        print(f'processed_geotiff  {file}','\n')
 
-        parent_folder = os.path.dirname(input_path)
-        output_dir = os.path.join(parent_folder, 'split', f'{timestamp}')
-        print(' Tiles created at  ',output_dir)
-        split_files(os.path.join(base_dir,files), output_dir)
+        parent_folder = os.path.dirname(processed_geotiff)
+        output_dir = os.path.join(parent_folder, 'SPLIT')
+        print(' Tiles created at  ',output_dir,'\n')
+        split_files(processed_geotiff, output_dir)
